a9cdfb0f01baabbb9807fd87a054c9dff1d30ffa
Kévin LACIRE <kevin.lacire@gmail.com>
2014-04-02 11:40:11 +0200

    little implementation & comments

--- src/miniEditeur/ActionNonReversible.java
+++ src/miniEditeur/ActionNonReversible.java
@@ -5,13 +5,30 @@ public abstract class ActionNonReversible implements Action{
 	private ActionReversible actionReversible;	
 	protected EditeurDeTexte editeurDeTexte;
 	
+	/**
+	 * Constructor of the class ActionNonReversible
+	 * @param e the editor to link with
+	 */
 	public ActionNonReversible(EditeurDeTexte e){
 		super();
 		editeurDeTexte=e;
 	}
 
+	/**
+	 * Method that send an order to cancel the execution of the linked ActionReversible
+	 */
 	public void executer() {
-		// TODO : to implement	
+		this.actionReversible.annuler();
+	}
+
+	// Accessors of actionReversible attribute
+	
+	public ActionReversible getActionReversible(){
+		return this.actionReversible;
+	}
+	
+	public void setActionReversible(ActionReversible ar){
+		this.actionReversible = ar;
 	}
 	
 }
