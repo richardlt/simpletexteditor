ff35753d5f24ac462e909cd6430667988c2abdcf
Kévin LACIRE <kevin.lacire@gmail.com>
2014-04-02 11:29:19 +0200

    Little implementation & comments

--- src/miniEditeur/Couper.java
+++ src/miniEditeur/Couper.java
@@ -2,17 +2,25 @@ package miniEditeur;
 
 public class Couper extends ActionReversible{
 	
+	/**
+	 * Constructor of the Couper class
+	 * @param e the editor to link with
+	 */
 	public Couper(EditeurDeTexte e){
 		super(e);
 	}
 
+	/**
+	 * Method that send an order to cut the selection in the text area into the paper press to the ZoneDeTravail
+	 */
 	public void executer() {
-		// TODO : to implement	
+		super.zoneDeTravail.couper();
 	}
 	
-	public void annuler() {
-		// TODO : to implement	
-	}
+	/**
+	 * Procedure to cancel effects of the cut action (reprint the selection into the text area to its right place)
+	 */
+	public void annuler() {}
 	
 }
 
