aec7dd0759ffe6a7abc5994f11a030e83736d966
Kevin Lacire <kevinthefirst@gmail.com>
2014-04-08 19:54:23 +0200

    Implementation of Paste Class + comments

--- src/miniEditeur/Coller.java
+++ src/miniEditeur/Coller.java
@@ -1,18 +1,29 @@
 package miniEditeur;
 
-public class Coller extends ActionReversible{
-	
-	public Coller(EditeurDeTexte e){
-		super(e);
-	}
+public class Coller extends ActionReversible {
 
-	public void executer() {
-		super.zoneDeTravail.coller();
-	}
-	
-	public void annuler() {
-		// TODO : to implement	
-	}
-	
+    /**
+     * Constructor of the Coller class
+     *
+     * @param e the editor to link with
+     */
+    public Coller(EditeurDeTexte e) {
+        super(e);
+    }
+
+    /**
+     * Method that send an order to paste the selection in into the
+     * Paper Press
+     */
+    public void executer() {
+        super.zoneDeTravail.coller();
+    }
+
+    /**
+     * Procedure to cancel effects of the paste action (unprint the selection into the text area and puts it in the PaperPress)
+     */
+    public void annuler() {
+        super.zoneDeTravail.effacer();
+    }
+
 }
-
