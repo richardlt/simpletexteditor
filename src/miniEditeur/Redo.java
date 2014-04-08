2f74d7e2c07ba71e91fa8a216c8e97ba24cd894e
Kevin Lacire <kevinthefirst@gmail.com>
2014-04-08 20:11:24 +0200

    Implementation & comments + new method in ZoneDeTravail => refaire to redo an Action

--- src/miniEditeur/Redo.java
+++ src/miniEditeur/Redo.java
@@ -1,14 +1,21 @@
 package miniEditeur;
 
-public class Redo extends ActionNonReversible{
-	
-	public Redo(EditeurDeTexte e){
-		super(e);
-	}
+public class Redo extends ActionNonReversible {
+    
+    /**
+     * Constructor of the Redo class
+     *
+     * @param e the editor to link with
+     */
+    public Redo(EditeurDeTexte e) {
+        super(e);
+    }
 
-	public void executer() {
-		
-	}
-	
+    /**
+     * Method that send an order to redo the next action
+     */
+    public void executer() {
+        super.editeurDeTexte.getZoneDeTravail().refaire();
+    }
+
 }
-
