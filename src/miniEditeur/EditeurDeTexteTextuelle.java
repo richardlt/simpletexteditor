10d82e3bffaaff7dd87239f3273760d19782bc8d
Richard <richard.le.terrier@gmail.com>
2014-04-08 22:25:59 +0200

    Evo

--- src/miniEditeur/EditeurDeTexteTextuelle.java
+++ src/miniEditeur/EditeurDeTexteTextuelle.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package miniEditeur;
 
 import java.util.Scanner;
@@ -67,62 +66,3 @@ public class EditeurDeTexteTextuelle extends EditeurDeTexte{
 	}
 
 }
-
-=======
-package miniEditeur;
-
-import java.util.Scanner;
-
-public class EditeurDeTexteTextuelle extends EditeurDeTexte{
-
-	public EditeurDeTexteTextuelle(){
-		super();		
-	}
-
-	public void run() {
-		while(true){
-			System.out.println("*******************************************************************************************************\n");
-			System.out.println(super.toString()+"\n");			
-			System.out.println("*******************************************************************************************************");
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Que voulez vous faire ?");
-			System.out.println("e : écrire du texte");
-			System.out.println("s : selectionner du texte");
-			System.out.println("cp : copier la selection");
-			System.out.println("p : coller la selection");
-			System.out.println("cu : couper la selection");
-			System.out.println("u : defaire");
-			System.out.println("r : refaire");
-			
-			String str = sc.nextLine();
-			
-			
-			if(str.equals("e")){
-				Action a=new Ecrire(this);
-				System.out.println("Ecrivez votre texte");
-				String text = sc.nextLine();
-				setTampon(text);
-				a.executer();
-			}else if(str.equals("s")){	
-				System.out.println("Ecrivez l'index de début de la sélection");
-				Integer start = sc.nextInt();
-				System.out.println("Ecrivez l'index de fin de la sélection");
-				Integer end = sc.nextInt();
-				setSelection(start, end);
-			}else if(str.equals("cp")){
-				
-			}else if(str.equals("p")){
-				
-			}else if(str.equals("cu")){
-				
-			}else if(str.equals("u")){
-				
-			}else if(str.equals("r")){
-				
-			}
-		}
-	}
-
-}
-
->>>>>>> branch 'master' of https://github.com/richardlt/simpletexteditor.git
--- src/miniEditeur/Test.java
+++ src/miniEditeur/Test.java
@@ -3,8 +3,8 @@ package miniEditeur;
 public class Test {
 
 	public static void main(String[] args) {
-		EditeurDeTexte edtg = new EditeurDeTexteGraphique();
-		edtg.start();
+		/*EditeurDeTexte edtg = new EditeurDeTexteGraphique();
+		edtg.start();*/
 		EditeurDeTexte edtt = new EditeurDeTexteTextuelle();
 		edtt.start();
 	}
