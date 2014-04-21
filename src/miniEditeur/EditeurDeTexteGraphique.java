1c704b81fce4b73044619e261556229b834afc1f
Richard <richard.le.terrier@gmail.com>
2014-04-21 10:03:33 +0200

    -ajout nouvelles fonctions dans l'interface graphique -simplification style de l'interface

--- src/miniEditeur/EditeurDeTexteGraphique.java
+++ src/miniEditeur/EditeurDeTexteGraphique.java
@@ -19,57 +19,54 @@ import javax.swing.border.Border;
 
 public class EditeurDeTexteGraphique extends EditeurDeTexte {
 
-    //protected static int startIndex, endIndex;
     /**
      * Constructor of the EditeurDeTexteGraphique's class
      */
     public EditeurDeTexteGraphique() {
         super();
-        //this.startIndex = 0;
-        //this.endIndex   = 0;
     }
 
     /**
      * Method called by the main function to print the UI
      */
-    @Override
     public void run() {
         JFrame fenetre = new JFrame();
         fenetre.setSize(800, 600);
         fenetre.setPreferredSize(fenetre.getSize());
         fenetre.setLayout(new BorderLayout());
-
+        
+        JPanel grid = new JPanel();
+        grid.setLayout(new GridLayout(1, 8));
+        
         // Configuration of action buttons 
         JButton buttonCopy = new JButton("Copier");
-        buttonCopy.setSize(30, 20);
-        buttonCopy.setBackground(Color.BLACK);
-        buttonCopy.setForeground(Color.WHITE);
         JButton buttonCut = new JButton("Couper");
-        buttonCut.setSize(30, 20);
-        buttonCut.setBackground(Color.BLUE);
-        buttonCut.setForeground(Color.WHITE);
         JButton buttonPaste = new JButton("Coller");
-        buttonPaste.setSize(30, 20);
-        buttonPaste.setBackground(Color.RED);
         JButton buttonUndo = new JButton("Annuler");
-        buttonUndo.setSize(30, 20);
-        buttonUndo.setBackground(Color.YELLOW);
         JButton buttonRedo = new JButton("Refaire");
-        buttonRedo.setSize(30, 20);
-        buttonRedo.setBackground(Color.ORANGE);
         JButton buttonSuppr = new JButton("Supprimer");
-        buttonSuppr.setSize(30, 20);
-        buttonSuppr.setBackground(Color.GRAY);
-
-        JPanel grid = new JPanel();
-        grid.setLayout(new GridLayout(1, 6));
+        JButton buttonRet = new JButton("Retour Arr");
+        JButton buttonRecord = new JButton("Record");
+        JButton buttonReplay = new JButton("Replay");
+        
+        grid.add(buttonCopy);
+        grid.add(buttonCut);
+        grid.add(buttonPaste);
+        grid.add(buttonSuppr);
+        grid.add(buttonRet);
+        grid.add(buttonUndo);
+        grid.add(buttonRedo);
+        grid.add(buttonRecord);
+        grid.add(buttonReplay);
+              
+        
         JPanel gridbottom = new JPanel();
-        final JTextPane tmpTextarea = new JTextPane();        
-        JButton buttonEcrire        = new JButton("Écrire");
-        buttonEcrire.setBackground(Color.GREEN);
+        gridbottom.setLayout(new GridLayout(1, 2));
+        final JTextPane tmpTextarea = new JTextPane();     
+        tmpTextarea.setBackground(Color.lightGray);
+        JButton buttonEcrire        = new JButton("Ecrire");        
         gridbottom.add(tmpTextarea);
         gridbottom.add(buttonEcrire);
-        gridbottom.setLayout(new GridLayout(1, 2));
         
 
         // Give access to the editor object in the listeners
@@ -85,9 +82,7 @@ public class EditeurDeTexteGraphique extends EditeurDeTexte {
             }
 
             public void actualiserContenu(){
-            	this.setText("");
-                this.setText(e.getZoneDeTravail().getBuffer().toString());                
-                System.out.println("actualiserContenu() - "+e.getZoneDeTravail().getBuffer().toString());
+            	this.setText(e.getZoneDeTravail().getBuffer().toString());                
             }
             
             public void actualiserSelection(){
@@ -96,10 +91,10 @@ public class EditeurDeTexteGraphique extends EditeurDeTexte {
         }
 
         final MyJTextArea zdt = new MyJTextArea(this);
-
+        zdt.setBackground(Color.WHITE);
+        
         // Add of action listener on every button created up there
         buttonCopy.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
                 zdt.actualiserSelection();
@@ -110,7 +105,6 @@ public class EditeurDeTexteGraphique extends EditeurDeTexte {
         });
 
         buttonCut.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {                
             	zdt.actualiserSelection();
@@ -121,7 +115,6 @@ public class EditeurDeTexteGraphique extends EditeurDeTexte {
         });
 
         buttonPaste.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
             	zdt.actualiserSelection();
@@ -132,7 +125,6 @@ public class EditeurDeTexteGraphique extends EditeurDeTexte {
         });
 
         buttonSuppr.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
             	zdt.actualiserSelection();
@@ -143,7 +135,6 @@ public class EditeurDeTexteGraphique extends EditeurDeTexte {
         });
 
         buttonUndo.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
             	zdt.actualiserSelection();
@@ -154,7 +145,6 @@ public class EditeurDeTexteGraphique extends EditeurDeTexte {
         });
         
         buttonRedo.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
             	zdt.actualiserSelection();
@@ -164,17 +154,7 @@ public class EditeurDeTexteGraphique extends EditeurDeTexte {
             }
         });
 
-        grid.add(buttonCopy);
-        grid.add(buttonCut);
-        grid.add(buttonPaste);
-        grid.add(buttonSuppr);
-        grid.add(buttonUndo);
-        grid.add(buttonRedo);
-
-        zdt.setBackground(Color.WHITE);
-
         buttonEcrire.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
             	zdt.actualiserSelection();
@@ -183,24 +163,49 @@ public class EditeurDeTexteGraphique extends EditeurDeTexte {
                 ecrire.executer();                
                 zdt.actualiserContenu();
             }
-        });        
+        });    
+        
+        buttonRecord.addActionListener(new ActionListener() {
+            private Boolean recordState=false;
+            public void actionPerformed(ActionEvent e) {
+            	if(!recordState){
+            		edt.startRecord();
+                	recordState=true;
+                	((JButton) e.getSource()).setText("Stop");
+            	}else{
+            		edt.stopRecord();
+                	recordState=false;                	
+                	((JButton) e.getSource()).setText("Record");
+            	}
+            }
+        }); 
+        
+        buttonReplay.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	edt.replay();
+            	zdt.actualiserContenu();
+            }
+        }); 
+        
+        buttonRet.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	zdt.actualiserSelection();
+            	RetourArriere ret = new RetourArriere(edt);edt.addAction(ret);
+            	ret.executer();                
+                zdt.actualiserContenu();
+            }
+        });
         
         /**
          * Constructor of the internal class MyKeyListener to handle writing events
          */
         class MyKeyListener implements KeyListener {
-
-            public void keyPressed(KeyEvent e) {}
-
-            public void keyReleased(KeyEvent e) {}
-
-            public void keyTyped(KeyEvent e) {
-            	e.consume();
-            }
+            public void keyPressed(KeyEvent e){}
+            public void keyReleased(KeyEvent e){}
+            public void keyTyped(KeyEvent e){e.consume();}
         }
 
         MyKeyListener kl = new MyKeyListener();
-
         zdt.addKeyListener(kl);
 
         fenetre.add(grid, BorderLayout.NORTH);
